c13c09971efe1a671aa0d71847a1dc02e93ba942
jleidel <john.leidel@gmail.com>
2021-02-11 07:00:54 -0600

    updating copyright for 2021

--- src/RevCPU.cc
+++ src/RevCPU.cc
@@ -1,7 +1,7 @@
 //
 // _RevCPU_cc_
 //
-// Copyright (C) 2017-2020 Tactical Computing Laboratories, LLC
+// Copyright (C) 2017-2021 Tactical Computing Laboratories, LLC
 // All Rights Reserved
 // contact@tactcomplabs.com
 //
--- src/insns/RV64I.h
+++ src/insns/RV64I.h
@@ -1,7 +1,7 @@
 //
 // _RV64I_h_
 //
-// Copyright (C) 2017-2020 Tactical Computing Laboratories, LLC
+// Copyright (C) 2017-2021 Tactical Computing Laboratories, LLC
 // All Rights Reserved
 // contact@tactcomplabs.com
 //
