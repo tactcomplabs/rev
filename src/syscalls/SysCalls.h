ac638704f60b4cb20c69d8f21e236a3272960fcb
Chris Taylor <ctaylor@tactcomplabs.com>
2022-01-26 16:04:29 -0500

    initial import syscalls uber header file

---
+++ src/syscalls/SysCalls.h
@@ -0,0 +1,22 @@
+
+//
+// SysCalls.h
+//
+// Copyright (C) 2017-2021 Tactical Computing Laboratories, LLC
+// All Rights Reserved
+// contact@tactcomplabs.com
+//
+// See LICENSE in the top level directory for licensing details
+//
+#pragma once
+#ifndef __SYSTEMCALLS_H__
+#define __SYSTEMCALLS_H__
+
+#include "SysCallExit.h"
+#include "SysCallExitGroup.h"
+#include "SysCallGetPid.h"
+#include "SysCallKill.h"
+
+#endif
+
+// EOF
