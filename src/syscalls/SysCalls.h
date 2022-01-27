8af6099b9da55148ca21eb061ea3d3bce73b8dac
Chris Taylor <ctaylor@tactcomplabs.com>
2022-01-27 14:19:16 -0500

    added system call code structure+enum

--- src/syscalls/SysCalls.h
+++ src/syscalls/SysCalls.h
@@ -18,6 +18,22 @@
 #include "SysCallKill.h"
 #include "SysCallTGKill.h"
 
+template<bool IsRiscv32>
+struct SystemCalls {
+
+    using RiscvModeIntegerType = typename std::conditional<IsRiscv32, std::uint32_t, std::uint64_t>::type;
+
+    enum Codes : RiscvModeIntegerType {
+        EXIT = ExitSystemCall<IsRiscv32>::code_value,
+        EXITGROUP = ExitGroupSystemCall<IsRiscv32>::code_value,
+        GETPID = GetPidSystemCall<IsRiscv32>::code_value,
+        KILL = KillSystemCall<IsRiscv32>::code_value,
+        TGKILL = TGKillSystemCall<IsRiscv32>::code_value,
+    };
+
+};
+
+
 #endif
 
 // EOF
