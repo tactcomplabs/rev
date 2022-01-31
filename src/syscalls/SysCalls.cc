cf9b6dae76f4010c38d8b79e8191fe2549a3e4c7
Chris Taylor <ctaylor@tactcomplabs.com>
2022-01-31 09:17:38 -0500

    added brk

--- src/syscalls/SysCalls.cc
+++ src/syscalls/SysCalls.cc
@@ -15,17 +15,17 @@ namespace SST { namespace RevCPU {
 
 template<typename RiscvArchType>
 std::unordered_map<typename SystemCalls<RiscvArchType>::Codes, SystemCallInterface<RiscvArchType>> SystemCalls<RiscvArchType>::jump_table {
-        { SystemCalls<RiscvArchType>::Codes::EXIT, ExitSystemCall<IsRiscv32>{} },
+        { SystemCalls<RiscvArchType>::Codes::EXIT     , ExitSystemCall<IsRiscv32>{} },
         { SystemCalls<RiscvArchType>::Codes::EXITGROUP, ExitGroupSystemCall<IsRiscv32>{} },
-        { SystemCalls<RiscvArchType>::Codes::GETPID, GetPidSystemCall<IsRiscv32>{} },
-        { SystemCalls<RiscvArchType>::Codes::KILL, KillSystemCall<IsRiscv32>{} },
-        { SystemCalls<RiscvArchType>::Codes::TGKILL, TGKillSystemCall<IsRiscv32>{} },
-        { SystemCalls<RiscvArchType>::Codes::READ, ReadSystemCall<IsRiscv32>{} },
-        { SystemCalls<RiscvArchType>::Codes::WRITE, WriteSystemCall<IsRiscv32>{} },
-        { SystemCalls<RiscvArchType>::Codes::OPENAT, OpenAtSystemCall<IsRiscv32>{} },
-        { SystemCalls<RiscvArchType>::Codes::CLOSE, OpenAtSystemCall<IsRiscv32>{} },
-        { SystemCalls<RiscvArchType>::Codes::LSEEK, OpenAtSystemCall<IsRiscv32>{} },
-        { SystemCalls<RiscvArchType>::Codes::BRK, OpenAtSystemCall<IsRiscv32>{} },
+        { SystemCalls<RiscvArchType>::Codes::GETPID   , GetPidSystemCall<IsRiscv32>{} },
+        { SystemCalls<RiscvArchType>::Codes::KILL     , KillSystemCall<IsRiscv32>{} },
+        { SystemCalls<RiscvArchType>::Codes::TGKILL   , TGKillSystemCall<IsRiscv32>{} },
+        { SystemCalls<RiscvArchType>::Codes::READ     , ReadSystemCall<IsRiscv32>{} },
+        { SystemCalls<RiscvArchType>::Codes::WRITE    , WriteSystemCall<IsRiscv32>{} },
+        { SystemCalls<RiscvArchType>::Codes::OPENAT   , OpenAtSystemCall<IsRiscv32>{} },
+        { SystemCalls<RiscvArchType>::Codes::CLOSE    , OpenAtSystemCall<IsRiscv32>{} },
+        { SystemCalls<RiscvArchType>::Codes::LSEEK    , OpenAtSystemCall<IsRiscv32>{} },
+        { SystemCalls<RiscvArchType>::Codes::BRK      , OpenAtSystemCall<IsRiscv32>{} },
 };
 
 } /* end namespace RevCPU */ } // end namespace SST
