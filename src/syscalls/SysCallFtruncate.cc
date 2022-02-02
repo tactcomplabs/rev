4188741abe25db5b561ac68cb11d117bda80901c
Chris Taylor <ctaylor@tactcomplabs.com>
2022-02-02 13:13:22 -0500

    added ftruncate

---
+++ src/syscalls/SysCallFtruncate.cc
@@ -0,0 +1,78 @@
+//
+// SysCallFtruncate.cc
+//
+// Copyright (C) 2017-2021 Tactical Computing Laboratories, LLC
+// All Rights Reserved
+// contact@tactcomplabs.com
+//
+// See LICENSE in the top level directory for licensing details
+//
+#include "SysCallFtruncate.h"
+
+#include <unistd.h>
+#include <signal.h>
+#include <sys/stat.h>
+
+namespace SST { namespace RevCPU {
+
+template<>
+template<>
+void FtruncateSystemCall<Riscv32>::invoke<int>(FtruncateSystemCall<Riscv32>::SystemCallParameterInterfaceType & parameters, int & value) {
+    if(parameters.count() == 2) {
+
+        int fildes;
+        off_t length;
+
+        bool has_values[2] = { false, false };
+        has_values[0] = parameters.get<int>(0, fd);
+        has_values[1] = parameters.get<off_t>(1, path);
+
+
+        if(has_values[0] && has_values[1]) {
+            success = true;
+            value = ftruncate(fildes, length);
+        }
+    }
+}
+
+template<>
+template<>
+void FtruncateSystemCall<Riscv64>::invoke<int>(FtruncateSystemCall<Riscv64>::SystemCallParameterInterfaceType & parameters, int & value) {
+    if(parameters.count() == 2) {
+
+        int fildes;
+        off_t length;
+
+        bool has_values[2] = { false, false };
+        has_values[0] = parameters.get<int>(0, fd);
+        has_values[1] = parameters.get<off_t>(1, path);
+
+
+        if(has_values[0] && has_values[1]) {
+            success = true;
+            value = ftruncate(fildes, length);
+        }
+    }
+}
+
+template<>
+template<>
+void FtruncateSystemCall<Riscv128>::invoke<int>(FtruncateSystemCall<Riscv128>::SystemCallParameterInterfaceType & parameters, int & value) {
+    if(parameters.count() == 2) {
+
+        int fildes;
+        off_t length;
+
+        bool has_values[2] = { false, false };
+        has_values[0] = parameters.get<int>(0, fd);
+        has_values[1] = parameters.get<off_t>(1, path);
+
+
+        if(has_values[0] && has_values[1]) {
+            success = true;
+            value = ftruncate(fildes, length);
+        }
+    }
+}
+
+} /* end namespace RevCPU */ } // end namespace SST
---
+++ src/syscalls/SysCallFtruncate.h
@@ -0,0 +1,101 @@
+//
+// SysCallFtruncate.h
+//
+// Copyright (C) 2017-2021 Tactical Computing Laboratories, LLC
+// All Rights Reserved
+// contact@tactcomplabs.com
+//
+// See LICENSE in the top level directory for licensing details
+//
+#pragma once
+#ifndef __SYSTEMCALLFTRUNCATE_H__
+#define __SYSTEMCALLFTRUNCATE_H__
+
+#include "SystemCallInterface.h"
+#include <type_traits>
+#include <string>
+#include <sys/types.h>
+#include <string>
+
+namespace SST { namespace RevCPU {
+
+template<typename RiscvArchType=Riscv32>
+using FtruncateSystemCallParametersInterfaceType = SystemCallInterface<RiscvArchType, 17>;
+
+template<typename RiscvArchType=Riscv32>
+class FtruncateSystemCallParameters : public virtual FtruncateSystemCallParametersInterfaceType<RiscvArchType> {
+    
+    private:
+
+    int fildes;
+    offset_t length;
+
+    public:
+
+    using SystemCallParameterInterfaceType = FtruncateSystemCallParametersInterfaceType<RiscvArchType>;
+    using SystemCallCodeType = typename SystemCallParameterInterfaceType::SystemCallCodeType;
+
+    FtruncateSystemCallParameters(int fildesp, offset_t lengthp)
+        : SystemCallParameterInterfaceType(), fildes(fildesp), length(lengthp) {}
+
+    size_t count() override {
+        return 2UL;
+    }
+
+    template<typename ParameterType>
+    bool get(const size_t parameter_index, ParameterType & param);
+
+    template<>
+    bool get(const size_t parameter_index, int& param) {
+        if(parameter_index == 0) {
+            param = fildes;
+            return true;
+        }
+
+        return false;
+    }
+
+    template<>
+    bool get(const size_t parameter_index, offset_t& param) {
+        if(parameter_index == 0) {
+            param = length;
+            return true;
+        }
+
+        return false;
+    }
+};
+
+template<typename RiscvArchType=Riscv32>
+using FtruncateSystemCallInterfaceType = SystemCallInterface<RiscvArchType, 17>;
+
+template<typename RiscvArchType=Riscv32>
+class FtruncateSystemCall : public virtual FtruncateSystemCallInterfaceType<RiscvArchType> {
+  
+    public:
+
+    using SystemCallInterfaceType = FtruncateSystemCallInterfaceType<RiscvArchType>;
+
+    using RiscvModeIntegerType = typename SystemCallInterfaceType::RiscvModeIntegerType;
+    using SystemCallCodeType = typename SystemCallInterfaceType::SystemCallCodeType;
+    
+    using SystemCallParameterInterfaceType = SystemCallParameterInterface<RiscvArchType, SystemCallInterfaceType::SystemCallCodeType::value>;    
+
+    FtruncateSystemCall() : SystemCallInterfaceType() {}
+
+    // always returns false
+    //
+    template<typename ReturnType>
+    void invoke(SystemCallParameterInterfaceType & parameters, ReturnType & value);
+
+    // returns true
+    //
+    template<>
+    void invoke(SystemCallParameterInterfaceType & parameters, int & value);
+};
+
+} /* end namespace RevCPU */ } // end namespace SST
+
+#endif
+
+// EOF
