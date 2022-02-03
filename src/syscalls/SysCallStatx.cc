482f5b17b4a09c480cb72a63cb9a08fd7738009d
Chris Taylor <ctaylor@tactcomplabs.com>
2022-02-03 14:22:12 -0500

    added statx

---
+++ src/syscalls/SysCallStatx.cc
@@ -0,0 +1,93 @@
+//
+// SysCallStatx.cc
+//
+// Copyright (C) 2017-2021 Tactical Computing Laboratories, LLC
+// All Rights Reserved
+// contact@tactcomplabs.com
+//
+// See LICENSE in the top level directory for licensing details
+//
+#include "SysCallStatx.h"
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+namespace SST { namespace RevCPU {
+
+template<>
+template<>
+void StatxSystemCall<Riscv32>::invoke<int>(StatxSystemCall<Riscv32>::SystemCallParameterInterfaceType & parameters, int & value) {
+    if(parameters.count() == 5) {
+
+        int dirfd;
+        const char * pathname;
+        int flags;
+        unsigned int mask;
+        statx * statxbuf;
+
+        bool has_values[5] = { false, false, false, false, false };
+        has_values[0] = parameters.get<int>(0, ofd);
+        has_values[1] = parameters.get<const char *>(1, nfd);
+        has_values[2] = parameters.get<int>(2, flags);
+        has_values[3] = parameters.get<unsigned int>(3, flags);
+        has_values[4] = parameters.get<statx *>(4, flags);
+
+        if(has_values[0] && has_values[1] && has_values[2] && has_values[3]  && has_values[4]) {
+            success = true;
+            value = statx(dirfd, pathname, flags, mask, statxbuf);
+        }
+    }
+}
+
+template<>
+template<>
+void StatxSystemCall<Riscv64>::invoke<int>(StatxSystemCall<Riscv64>::SystemCallParameterInterfaceType & parameters, int & value) {
+    if(parameters.count() == 5) {
+
+        int dirfd;
+        const char * pathname;
+        int flags;
+        unsigned int mask;
+        statx * statxbuf;
+
+        bool has_values[5] = { false, false, false, false, false };
+        has_values[0] = parameters.get<int>(0, ofd);
+        has_values[1] = parameters.get<const char *>(1, nfd);
+        has_values[2] = parameters.get<int>(2, flags);
+        has_values[3] = parameters.get<unsigned int>(3, flags);
+        has_values[4] = parameters.get<statx *>(4, flags);
+
+        if(has_values[0] && has_values[1] && has_values[2] && has_values[3]  && has_values[4]) {
+            success = true;
+            value = statx(dirfd, pathname, flags, mask, statxbuf);
+        }
+    }
+}
+
+template<>
+template<>
+void StatxSystemCall<Riscv128>::invoke<int>(StatxSystemCall<Riscv128>::SystemCallParameterInterfaceType & parameters, int & value) {
+    if(parameters.count() == 5) {
+
+        int dirfd;
+        const char * pathname;
+        int flags;
+        unsigned int mask;
+        statx * statxbuf;
+
+        bool has_values[5] = { false, false, false, false, false };
+        has_values[0] = parameters.get<int>(0, ofd);
+        has_values[1] = parameters.get<const char *>(1, nfd);
+        has_values[2] = parameters.get<int>(2, flags);
+        has_values[3] = parameters.get<unsigned int>(3, flags);
+        has_values[4] = parameters.get<statx *>(4, flags);
+
+        if(has_values[0] && has_values[1] && has_values[2] && has_values[3]  && has_values[4]) {
+            success = true;
+            value = statx(dirfd, pathname, flags, mask, statxbuf);
+        }
+    }
+}
+
+} /* end namespace RevCPU */ } // end namespace SST
---
+++ src/syscalls/SysCallStatx.h
@@ -0,0 +1,125 @@
+//
+// SysCallStatx.h
+//
+// Copyright (C) 2017-2021 Tactical Computing Laboratories, LLC
+// All Rights Reserved
+// contact@tactcomplabs.com
+//
+// See LICENSE in the top level directory for licensing details
+//
+#pragma once
+#ifndef __SYSTEMCALLSTATX_H__
+#define __SYSTEMCALLSTATX_H__
+
+#include "SystemCallInterface.h"
+#include <type_traits>
+#include <sys/types.h>
+#include <fcntl.h>
+
+namespace SST { namespace RevCPU {
+
+template<typename RiscvArchType=Riscv32>
+using StatxSystemCallParametersInterfaceType = SystemCallInterface<RiscvArchType, 93>;
+
+template<typename RiscvArchType=Riscv32>
+class StatxSystemCallParameters : public virtual StatxSystemCallParametersInterfaceType<RiscvArchType> {
+    
+    private:
+
+    int dirfd;
+    const char * pathname;
+    int flags;
+    unsigned int mask;
+    statx * statxbuf;
+
+    public:
+
+    using SystemCallParameterInterfaceType = StatxSystemCallParametersInterfaceType<RiscvArchType>;
+    using SystemCallCodeType = typename SystemCallParameterInterfaceType::SystemCallCodeType;
+
+    StatxSystemCallParameters(int dirfdp, const char * pathnamep, int flagsp, unsigned int maskp, statx * statxbufp)
+        : SystemCallParameterInterfaceType(), dirfd(dirfdp), flags(flagsp), mask(maskp), statxbuf(statxbuf) {}
+
+    size_t count() override { return 5UL; }
+
+    template<typename ParameterType>
+    bool get(const size_t parameter_index, ParameterType & param);
+
+    template<>
+    bool get(const size_t parameter_index, int& param) {
+        if(parameter_index == 0) {
+            param = dirfd;
+            return true;
+        }
+        else if(parameter_index == 2) {
+            param = flags;
+            return true;
+        }
+
+        return false;
+    }
+
+    template<>
+    bool get(const size_t parameter_index, const char* & param) {
+        if(parameter_index == 1) {
+            param = pathname;
+            return true;
+        }
+
+        return false;
+    }    
+
+    template<>
+    bool get(const size_t parameter_index, unsigned int & param) {
+        if(parameter_index == 3) {
+            param = mask;
+            return true;
+        }
+        
+        return false;
+    }        
+
+    template<>
+    bool get(const size_t parameter_index, statx * & param) {
+        if(parameter_index == 4) {
+            param = statxbuf;
+            return true;
+        }
+        
+        return false;
+    }            
+};
+
+template<typename RiscvArchType=Riscv32>
+using StatxSystemCallInterfaceType = SystemCallInterface<RiscvArchType, 93>;
+
+template<typename RiscvArchType=Riscv32>
+class StatxSystemCall : public virtual StatxSystemCallInterfaceType<RiscvArchType> {
+  
+    public:
+
+    using SystemCallInterfaceType = StatxSystemCallInterfaceType<RiscvArchType>;
+
+    using RiscvModeIntegerType = typename SystemCallInterfaceType::RiscvModeIntegerType;
+    using SystemCallCodeType = typename SystemCallInterfaceType::SystemCallCodeType;
+    
+    using SystemCallParameterInterfaceType = SystemCallParameterInterface<RiscvArchType, SystemCallInterfaceType::SystemCallCodeType::value>;    
+
+    StatxSystemCall() : SystemCallInterfaceType() {}
+
+    // always returns false
+    //
+    template<typename ReturnType>
+    void invoke(SystemCallParameterInterfaceType & parameters, ReturnType & value);
+
+    // returns true
+    //
+    template<>
+    void invoke(SystemCallParameterInterfaceType & parameters, int & value);
+};
+
+} /* end namespace RevCPU */ } // end namespace SST
+
+#endif
+
+// EOF
