190b916fdafe92f736322a6c8820d198932591b8
jleidel <john.leidel@gmail.com>
2022-11-23 18:07:55 -0600

    expanding python support

--- src/pyrevcpu.py
+++ src/pyrevcpu.py
@@ -16,9 +16,35 @@ from sst.merlin.base import *
 class RevCPU():
   def __init__(self):
     print("Initializing RevCPU")
+    self.declareClassVariables(["_numCores"])
+    self._numCPU = 0
 
   def getName(self):
     return "RevCPU"
+
+  def getNumCPU(self):
+    return self._numCPU
+
+  def enableRevSim(self,cpus,cores,clock,memSize,machine,startAddr,memCost,program,verbose):
+    print("Enabling Rev CPU component params")
+    self._numCPU = cpus
+    for i in range(0, cpus)
+      print("Building RevCPU " + str(i))
+      sst.pushNamePrefix("CPU"+str(i))
+
+      # build component name = "CPUx.rev"
+      cpu = sst.Component("rev","revcpu.RevCPU")
+      cpu.addParam("verbose", verbose)
+      cpu.addParam("numCores", cores)
+      cpu.addParam("clock", clock)
+      cpu.addParam("memSize", memSize)
+      cpu.addParam("machine",machine)
+      cpu.addParam("startAddr",startAdd)
+      cpu.addParam("memCost",memCost)
+      cpu.addParam("program",program)
+      _CPUS.append(cpu)
+      sst.popNamePrefix()
+
 
   def enableStatistics(self, level):
     print("Enabling Rev statistics")
@@ -26,14 +52,21 @@ class RevCPU():
     sst.setStatisticsOutput("sst.statOutputCSV")
     sst.enableAllStatisticsForComponentType("revcpu.RevCPU")
 
-class RevJob(Job):
-  def __init__(self, job_id, num_nodes, mem_size, num_cores = 1, cpu_type="RV64IMAFDC"):
-    Job.__init__(self,job_id,num_nodes)
-    self.declareClassVariables(["_numCores", "_memSize", "_cpuType"])
+  #-- private variables
+  _CPUS = []
 
-    self._numCores = num_cores
+class RevJob(Job):
+  def __init__(self, job_id, num_nodes):
+    Job.__init__(self,job_id,num_nodes)
+    _rev = RevCPU()
+
+    self.declareClassVariables(["_memSize", "_cpuType"])
+
     self._memSize = mem_size
     self._cpuType = cpu_type
+
+  def initCPUS(self,cores,clock,memSize,machine,startAddr,memCost,program,verbose):
+    _rev.enableRevSim(self,_num_nodes,cores,clock,memSize,machine,startAddr,memCost,program,verbose)
 
   def getName(self):
     return "RevJob"
@@ -50,5 +83,8 @@ class RevJob(Job):
     retval = ( networkif, port_name )
 
     return retval
+
+  #-- private variables
+  _rev
 
 #-- EOF
