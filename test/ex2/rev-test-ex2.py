8bd58afda34cd2a2c8ffc3613791f2b06b57be36
David Donofrio <ddonofrio@tactcomplabs.com>
2023-02-28 17:09:57 -0800

    adding tests to CTest

--- test/ex2/rev-test-ex2.py
+++ test/ex2/rev-test-ex2.py
@@ -28,7 +28,7 @@ comp_cpu.addParams({
 	"clock" : "1.0GHz",                           # Clock
         "memSize" : 1024*1024*1024,                   # Memory size in bytes
         "machine" : "[0:RV64G]",                      # Core:Config; RV64I for core 0
-        "startAddr" : "[0:0x0001014c]",               # Starting address for core 0
+        "startAddr" : "[0:0x00000000]",               # Starting address for core 0
         "memCost" : "[0:1:10]",                       # Memory loads required 1-10 cycles
         "program" : os.getenv("REV_EXE", "ex2.exe"),  # Target executable
         "splash" : 1                                  # Display the splash message
