c334d93574a81ce718ac686754b2e3b6e170f428
ctaylor <ctaylor@tactcomplabs.com>
2023-03-31 11:11:21 -0400

    initial write + pwrite support

--- src/RevSysCalls.cc
+++ src/RevSysCalls.cc
@@ -1,5 +1,7 @@
 #include "../include/syscalls/RevChdir.h"
 #include "../include/syscalls/RevMkdirat.h"
+#include "../include/syscalls/RevWrite.h"
+#include "../include/syscalls/RevPwrite.h"
 #include "../include/RevSysCalls.h"
 
 #define _SYSCALL_DEBUG_ 1 
@@ -7,8 +9,12 @@
 std::unordered_map<int, systemcall_t> SystemCalls::jump_table32 = {
   { RevChdir::value, RevChdir::ECall<Riscv32> },
   { RevMkdirat::value, RevMkdirat::ECall<Riscv32> },
+  { RevWrite::value, RevWrite::ECall<Riscv32> },
+  { RevPwrite::value, RevPwrite::ECall<Riscv32> },
 };
 std::unordered_map<int, systemcall_t> SystemCalls::jump_table64 = {
   { RevChdir::value, RevChdir::ECall<Riscv64> },
   { RevMkdirat::value, RevMkdirat::ECall<Riscv64> },
+  { RevWrite::value, RevWrite::ECall<Riscv64> },
+  { RevPwrite::value, RevPwrite::ECall<Riscv64> },
 };
