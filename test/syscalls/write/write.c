c416a66af6388228e1bc773bcb71c5852b27c511
Ryan Kabrick <rkabrick@udel.edu>
2023-05-15 23:22:59 +0000

    BROKEN: Write is infinite looping with unknown-elf

--- test/syscalls/write/write.c
+++ test/syscalls/write/write.c
@@ -1,19 +1,12 @@
+#include "../../../common/syscalls.h"
 #include <unistd.h>
 
 int main() {
-  // char msg[3];
-  // msg[0] = 'T';
-  // msg[1] = 'T';
-  // msg[2] = '\0';
 
-  // asm volatile("addi s2, zero, 72");
-  // asm volatile("addi s3, zero, 69");
-  // asm volatile("addi s4, zero, 0");
+  const char msg[10] = "Greetings"; // {72, 69, 0};
+  size_t msg_len = 10;                       // Length of the message string, including the newline character
 
-  const char msg[3] = "HE";// {72, 69, 0};
-  size_t msg_len = 3; // Length of the message string, including the newline character
-
-  ssize_t bytes_written = write(STDOUT_FILENO, &msg, msg_len);
+  ssize_t bytes_written = rev_write(STDOUT_FILENO, &msg, msg_len);
 
   // if (1){// (bytes_written < 0) {
   //   return 1;
