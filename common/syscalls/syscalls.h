f198928722eb9d7c6bcebf919c00a27bfdb5338f
Ryan Kabrick <rkabrick@udel.edu>
2023-05-18 20:07:04 +0000

    fixing bug where all system calls return 0
    Because the function definitions in syscalls.h all originally were returning 0, the `a0` register (function return argument) would be overwritten by 0 everytime.
    
    Now, there is an additional mv instruction which places the value in a0 (after the ecall) into a variable `rc` and that is what is returned.
    This *might* half-work if we just didn't return anything but then it wouldn't be possible to write code that depends on the return value of a system call (ie. fork)
    because you wouldn't be able to access the pid that's returned from a void function

--- common/syscalls/syscalls.h
+++ common/syscalls/syscalls.h
@@ -55,293 +55,369 @@ struct clone_args {
 
 
 uint64_t rev_getcwd(char *buf, unsigned long size){          
+  uint64_t rc;
     asm volatile (
       "li a7, 17\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_dup(unsigned int fildes){             
+  uint64_t rc;
     asm volatile (
       "li a7, 23\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_dup3(unsigned int oldfd, unsigned int newfd, int flags){            
+  uint64_t rc;
     asm volatile (
       "li a7, 24\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_mkdirat(int dfd, const char * path, unsigned short mode){         
+  uint64_t rc;
     asm volatile (
       "li a7, 34\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_chdir(const char *filename){
+  uint64_t rc;
   
   asm volatile (
     "li a7, 49\n\t"
     "ecall\n\t"
+    "mv %0, a0" : "=r" (rc)
   );
-  return 0;
+  return rc;
 }
 
 uint64_t rev_fchownat(int dfd, const char *filename, unsigned user, unsigned group, int flag){        
+  uint64_t rc;
     asm volatile (
       "li a7, 54\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+  return rc;
 }
 
 uint64_t rev_fchown(unsigned int fd, unsigned short user, unsigned short group){          
+  uint64_t rc;
     asm volatile (
       "li a7, 55\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+  return rc;
 }
 
 uint64_t rev_close(unsigned int fd){
+  uint64_t rc;
     asm volatile (
       "li a7, 57\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+  return rc;
 }
 
 uint64_t rev_read(unsigned int fd, char *buf, size_t nbytes){
+  uint64_t rc;
     asm volatile (
       "li a7, 63\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+  return rc;
 }
 
 uint64_t rev_write(unsigned int fd, const char *buf, size_t nbytes){          
+  uint64_t rc;
     asm volatile (
       "li a7, 64\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
 
-uint64_t rev_openat(int dfd, const char *filename, int flags, unsigned short mode){          
+uint64_t rev_openat(int dfd, const char *filename,  unsigned short mode){          
+  uint64_t rc;
     asm volatile (
       "li a7, 65\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_tee(int fdin, int fdout, size_t len, unsigned int flags){             
+  uint64_t rc;
     asm volatile (
       "li a7, 77\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_sync(void){            
+  uint64_t rc;
     asm volatile (
       "li a7, 81\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_fsync(unsigned int fd){           
+  uint64_t rc;
     asm volatile (
       "li a7, 82\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_fdatasync(unsigned int fd){       
+  uint64_t rc;
     asm volatile (
       "li a7, 83\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_exit(int error_code){           
+  uint64_t rc;
     asm volatile (
       "li a7, 93\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_exit_group(int error_code){      
+  uint64_t rc;
     asm volatile (
       "li a7, 94\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_waitid(int which, pid_t pid, struct siginfo  *infop, int options, struct rusage  *ru){          
+  uint64_t rc;
     asm volatile (
       "li a7, 95\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_set_robust_list(struct robust_list_head *head, size_t len){ 
+  uint64_t rc;
     asm volatile (
       "li a7, 99\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_get_robust_list(int pid, struct robust_list_head *head_ptr, size_t *len_ptr){ 
+  uint64_t rc;
     asm volatile (
       "li a7, 100\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_nanosleep(struct __kernel_timespec *rqtp, struct __kernel_timespec *rmtp){
+  uint64_t rc;
     asm volatile (
       "li a7, 101\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_timer_create(clockid_t which_clock, struct sigevent *timer_event_spec, timer_t *created_timer_id){
+  uint64_t rc;
     asm volatile (
       "li a7, 107\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_timer_delete(timer_t timer_id){    
+  uint64_t rc;
     asm volatile (
       "li a7, 110\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_rt_sigprocmask(int how, sigset_t *set,
                             sigset_t *oset, size_t sigsetsize){  
-    asm volatile (
-      "li a7, 135\n\t"
-      "ecall \n\t"
-    );
-    return 0;
+  uint64_t rc;
+  asm volatile (
+    "li a7, 135\n\t"
+    "ecall \n\t"
+    "mv %0, a0" : "=r" (rc)
+  );
+  return rc;
 }
 
 uint64_t rev_gettimeofday(struct timeval *tv, struct timezone *tz){    
+  uint64_t rc;
     asm volatile (
       "li a7, 169\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_settimeofday(struct timeval *tv, struct timezone *tz){    
+  uint64_t rc;
     asm volatile (
       "li a7, 170\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
+
+
 uint64_t rev_getpid(void){         
+  uint64_t rc;
     asm volatile (
       "li a7, 172\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
+
+
 uint64_t rev_getppid(void){        
+  uint64_t rc;
     asm volatile (
       "li a7, 173\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
 
+
 uint64_t rev_gettid(void){          
+  uint64_t rc;
     asm volatile (
       "li a7, 178\n\t"
       "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
 
 
 uint64_t rev_fork() {
-
-    asm volatile (
-        "li a7, 220\n\t"     // load the value 220 into a7
-        "ecall\n\t"       // execute the ecall instruction
-    );
-    return 0;
+  uint64_t rc;
+  asm volatile (
+      "li a7, 220\n\t"     // load the value 220 into a7
+      "ecall\n\t"       // execute the ecall instruction
+      "mv %0, a0" : "=r" (rc)
+  );
+  return rc;
 }
 
 uint64_t rev_clone3(struct clone_args* args, size_t args_size) {
+  uint64_t rc;
 
     asm volatile (
         "li a7, 220\n\t"     // load the value 220 into a7
         "ecall\n\t"       // execute the ecall instruction
+      "mv %0, a0" : "=r" (rc)
     );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_mmap(struct mmap_arg_struct *args){            
+  uint64_t rc;
     asm volatile (
       "li a7, 222\n\t"
         "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
       );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_clock_gettime(clockid_t which_clock, struct timeval *tp){
+  uint64_t rc;
     asm volatile (
       "li a7, 403\n\t"
         "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
       );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_clock_settime(clockid_t which_clock, const struct __kernel_timespec *tp){   
+  uint64_t rc;
     asm volatile (
       "li a7, 404\n\t"
         "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
       );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_timer_gettime(timer_t timer_id, struct __kernel_itimerspec *setting){   
+  uint64_t rc;
     asm volatile (
       "li a7, 408\n\t"
         "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
       );
-    return 0;
+    return rc;
 }
 
 uint64_t rev_timer_settime(timer_t timer_id, int flags, const struct __kernel_itimerspec  *new_setting, struct __kernel_itimerspec  *old_setting){   
+  uint64_t rc;
     asm volatile (
       "li a7, 409\n\t"
         "ecall \n\t"
+      "mv %0, a0" : "=r" (rc)
       );
-    return 0;
+    return rc;
 }
 
