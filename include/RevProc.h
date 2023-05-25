b4394bd7faf32d2aaac50580fb9953eaea54fd7c
Ryan Kabrick <rkabrick@udel.edu>
2023-05-25 18:16:30 +0000

    Added Thread Capabilities & System Calls

--- include/RevProc.h
+++ include/RevProc.h
@@ -26,6 +26,7 @@
 #include <time.h>
 #include <random>
 #include <queue>
+#include <functional>
 #include <inttypes.h>
 
 // -- RevCPU Headers
@@ -37,6 +38,8 @@
 #include "RevInstTables.h"
 #include "PanExec.h"
 #include "RevPrefetcher.h"
+#include "RevThreadCtx.h"
+#include "../common/syscalls/SysFlags.h"
 
 #define _PAN_FWARE_JUMP_            0x0000000000010000
 
@@ -93,6 +96,9 @@ namespace SST{
       /// RevProc: Handle ALU faults
       void HandleALUFault(unsigned width);
 
+      /// RevProc: Initialize ThreadTable & First Thread
+      bool InitThreadTable();
+
       class RevProcStats {
         public:
           uint64_t totalCycles;
@@ -108,6 +114,58 @@ namespace SST{
 
       RevProcStats GetStats();
 
+      RevMem& GetMem(){ return *mem; }
+
+      /* Software Process Table */
+      /* NOTE: Not all of these are needed/work */
+      bool AddCtx(RevThreadCtx& Ctx);
+      uint32_t CreateChildCtx(); // returns the childs pid
+      ThreadState GetThreadState(uint32_t pid);
+      bool SetState(ThreadState, uint32_t pid);
+      bool PauseThread(uint32_t pid);
+      bool ReadyThread(uint32_t pid);
+
+      /* Returns the current HartToExec active pid */
+      uint32_t GetActivePID(){ return ActivePIDs.at(HartToExec); } 
+      /* Returns the active pid for HartID */
+      uint32_t GetActivePID(const uint32_t HartID){ return ActivePIDs.at(HartID); } 
+
+      /* Returns the current HartToExec active pid */
+      // void UpdateRegFile(const uint32_t NewPID){ RegFiles.at(HartToExec) = ThreadTable.at(NewPID).GetRegFile();}
+
+      /* Returns the active pid for HartID */
+      // RevRegFile* GetRegFile(uint32_t pid){ return ThreadTable.at(pid).RegFile; }
+
+      const char&& GetThreadMemRef(uint32_t pid);
+  
+      std::vector<uint32_t> GetPIDs();
+
+      uint32_t RetireAndSwap(); // Returns new pid
+      
+      bool UpdateCtx(); // FIXME: This will change when RegFiles get changed to pointers
+      
+      std::unordered_map<uint32_t, std::shared_ptr<RevThreadCtx>> ThreadTable; ///< RevProc: PIDs & corresponding RevThreadCtx objects (Software Threads)
+      
+      bool LoadCtx(uint32_t pid);
+    
+    //   RevThreadCtx& GetCtx(uint32_t PID){ return ThreadTable.at(PID); }
+    //   RevThreadCtx& GetActiveCtx(){ return ThreadTable.at(ActivePIDs.at(HartToExec));}; ///< RevProc: Get the active ThreadCtx of HartToExec
+    //   RevThreadCtx& GetActiveCtx(uint8_t HartID){return ThreadTable.at(ActivePIDs.at(HartID)); } ///< RevProc: Get the active ThreadCtx of HartID
+
+      void CtxSwitchAlert(uint32_t NewPID) { NextPID=NewPID;PendingCtxSwitch = true; }
+
+      uint32_t HartToExecPID();
+      uint32_t HartToDecodePID();
+
+      std::shared_ptr<RevThreadCtx> HartToExecCtx();
+      std::shared_ptr<RevThreadCtx> HartToDecodeCtx();
+
+      bool ChangeActivePID(uint32_t PID); ///< RevProc: Change HartToExec active pid
+      bool ChangeActivePID(uint32_t PID, uint16_t HartID); ///< RevProc: Change HartID active pid
+
+      bool UpdateRegFile();
+      bool UpdateRegFile(uint16_t HartID);
+
     private:
       bool Halted;              ///< RevProc: determines if the core is halted
       bool Stalled;             ///< RevProc: determines if the core is stalled on instruction fetch
@@ -117,9 +175,12 @@ namespace SST{
       unsigned fault_width;     ///< RevProc: the width of the target fault
       unsigned id;              ///< RevProc: processor id
       uint64_t ExecPC;          ///< RevProc: executing PC
-      uint8_t threadToDecode;   ///< RevProc: Current executing ThreadID
-      uint8_t threadToExec;     ///< RevProc: Thread to dispatch instruction
+      uint16_t HartToDecode;   ///< RevProc: Current executing ThreadID
+      uint16_t HartToExec;     ///< RevProc: Thread to dispatch instruction
       uint64_t Retired;         ///< RevProc: number of retired instructions
+      bool PendingCtxSwitch = false; ///< RevProc: determines if the core is halted
+      bool SwapToParent = false; ///< RevProc: determines if the core is halted
+      uint32_t NextPID = 0; 
 
       RevOpts *opts;            ///< RevProc: options object
       RevMem *mem;              ///< RevProc: memory object
@@ -130,7 +191,63 @@ namespace SST{
       RevProcStats Stats;       ///< RevProc: collection of performance stats
       RevPrefetcher *sfetch;    ///< RevProc: stream instruction prefetcher
 
-      RevRegFile RegFile[_REV_THREAD_COUNT_];      ///< RevProc: register file
+      RevRegFile* RegFile = nullptr;
+
+      /*
+      * New Ecall Stuff
+      */
+      void ECALL_setxattr(); // 5
+      void ECALL_clone(); // 220
+      void ECALL_gettimeofday(); // 169
+      void ECALL_settimeofday(); // 170
+      void ECALL_getpid(); // 172
+      void ECALL_getppid(); // 173
+      void ECALL_gettid();  // 178
+      void ECALL_tee(); // 77
+      void ECALL_sync(); // 81
+      void ECALL_fsync(); // 82
+      void ECALL_fdatasync(); // 83
+      void ECALL_fchownat(); // 54
+      void ECALL_fchown(); // 55
+      void ECALL_getcwd(); // 17
+      void ECALL_chdir(); // 49
+      void ECALL_mkdir(); // Uses mkdirat under the hood
+      void ECALL_mkdirat();
+      void ECALL_read(); // 63
+      void ECALL_write(); // 64
+      void ECALL_waitid(); // 95
+      void ECALL_open(); // uses open under the hood
+      void ECALL_openat(); // 65
+      void ECALL_close(); // 57
+      void ECALL_mmap(); // 222
+      void ECALL_dup(); // 23 
+      void ECALL_dup3(); // 24
+      void ECALL_exit(); // 93
+      void ECALL_exit_group(); // 94
+      void ECALL_rt_sigprocmask(); // 135
+      void ECALL_set_robust_list(); // 99
+      void ECALL_get_robust_list(); // 100
+      void ECALL_nanosleep(); // 101
+      void ECALL_timer_create(); // 107
+      void ECALL_timer_delete(); // 110
+      void ECALL_clock_gettime(); // 403
+      void ECALL_clock_settime(); // 404
+      void ECALL_timer_gettime(); // 408
+      void ECALL_timer_settime(); // 409
+
+
+      void DumpARegs(bool is32);
+      std::unordered_map<uint32_t, std::function<void(RevProc*)>> Ecalls;
+      void InitEcallTable();
+      void ExecEcall();
+
+      // std::vector<RevRegFile*> RegFiles; // TODO: Maybe rename
+      RevRegFile* GetRegFile(uint16_t HartID);
+      // Return current executing HART regfile
+      // RevRegFile* RegFile();
+      
+      std::vector<uint32_t> ActivePIDs;
+
       RevInst Inst;             ///< RevProc: instruction payload
 
       std::vector<RevInstEntry> InstTable;        ///< RevProc: target instruction table
@@ -247,7 +364,7 @@ namespace SST{
       void ResetInst(RevInst *Inst);
 
       /// RevProc: Determine next thread to execute
-      uint16_t GetThreadID();
+      uint16_t GetHartID();
 
       /// RevProc: Check scoreboard for pipeline hazards
       bool DependencyCheck(uint16_t threadID, RevInst* Inst);
