0112c5fc9d66992bb121f6aaedcb57fb688bf390
Ryan Kabrick <rkabrick@udel.edu>
2023-06-13 15:43:50 +0000

    Fixing thread switch string that is output on thread completion
    - Was incorrectly outputting a message about switching to a threads parent when not actually doing so. Moved parent string output past the check if a thread has a parent.

--- src/RevProc.cc
+++ src/RevProc.cc
@@ -2091,10 +2091,10 @@ bool RevProc::ClockTick( SST::Cycle_t currentCycle ){
         uint32_t CurrPID = ActivePIDs.at(HartToExec);
         uint32_t ParentPID = ThreadTable.at(ActivePIDs.at(HartToExec))->GetParentPID();
         output->verbose(CALL_INFO, 2, 0,
-                      "Thread %u finished execution. Switching to its parent PID = %u", CurrPID, ParentPID);
+                      "Thread %u completed execution.\n", CurrPID);
         if(ParentPID != 0 ){
           done = false;
-          output->verbose(CALL_INFO, 2, 0, "Switching from thread with PID = %u to PID = %u\n", ActivePIDs.at(HartToExec), ParentPID);
+          output->verbose(CALL_INFO, 2, 0, "Switching from thread with PID = %u to its parent PID = %u\n", ActivePIDs.at(HartToExec), ParentPID);
           CtxSwitchAlert(ParentPID);
           SwapToParent = true;
           ThreadTable.at(ActivePIDs.at(HartToExec))->SetState(ThreadState::Dead);
