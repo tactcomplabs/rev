6047bebd263fe94e98d1719e3423ca7ac64955c1
jleidel <john.leidel@gmail.com>
2023-06-30 12:52:09 -0500

    temporarily using 128B cache lines

--- test/cache_test2/rev-test-cache2.py
+++ test/cache_test2/rev-test-cache2.py
@@ -62,7 +62,7 @@ l1cache.addParams({
     "replacement_policy" : "lru",
     "coherence_protocol" : "MESI",
     "associativity" : "4",
-    "cache_line_size" : "64",
+    "cache_line_size" : "128",
     "debug" : 1,
     "debug_level" : DEBUG_LEVEL,
     "verbose" : VERBOSE,
