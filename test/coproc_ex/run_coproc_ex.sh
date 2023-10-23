616850ee443606ebfc89ef1fcb331c3c644c832b
jleidel <john.leidel@gmail.com>
2023-10-23 10:09:36 -0500

    fixing coproc_ex test

--- test/coproc_ex/run_coproc_ex.sh
+++ test/coproc_ex/run_coproc_ex.sh
@@ -6,7 +6,7 @@ make
 # Check that the exec was built...
 if [ -f ex1.exe ]; then
   sst-info revcpu
-  sst ./rev-test-coproc_ex.py
+  sst --add-lib-path=../../build/src/ ./rev-test-coproc_ex.py
 else
   echo "Test COPROC: File ex1.exe not found - likely build failed"
   exit 1
