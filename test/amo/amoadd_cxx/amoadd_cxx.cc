d5941ea439426a9c18e377a392ce2dcd57d224e2
David Donofrio <ddonofrio@tactcomplabs.com>
2023-11-10 18:55:39 -0800

    restoring amoadd... again

--- test/amo/amoadd_cxx/amoadd_cxx.cc
+++ test/amo/amoadd_cxx/amoadd_cxx.cc
@@ -1,9 +1,7 @@
 #include <atomic>
 #define assert(x) if (!(x)) { asm(".byte 0x00"); asm(".byte 0x00"); asm(".byte 0x00"); asm(".byte 0x00"); }
-#include "../../include/rev-macros.h"
 
 int main() {
-        TRACE_ON;
         std::atomic<int> a;
         a = 1;  //amoswap
         assert(a == 1);
@@ -11,9 +9,8 @@ int main() {
         assert(a == 2);
 
         std::atomic<unsigned long long> b;
-        b = 0xDEADBEEF;  //amoswap
-        assert(b == 0xDEADBEEF);
+        b = 1;  //amoswap
+        assert(b == 1);
         b++;    //amoadd
-        assert(b == 0xDEADBEF0);
-        TRACE_OFF;
+        assert(b == 2);
 }
