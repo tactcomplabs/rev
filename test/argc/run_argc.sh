dd1ef1bfbedcd4d6dabb5fe5b8dd726234db9d6d
Ryan Kabrick <rkabrick@udel.edu>
2023-11-22 13:52:14 -0500

    renaming main rev python config

--- test/argc/run_argc.sh
+++ test/argc/run_argc.sh
@@ -5,9 +5,9 @@ make clean && make
 
 # Check that the exec was built...
 if [ -f argc.exe ]; then
-	sst --add-lib-path=../../build/src/ ../rev-basic-config.py -- --program "argc.exe" --args "one two three" --enableMemH=0
+	sst --add-lib-path=../../build/src/ ../rev-model-options-config.py -- --program "argc.exe" --args "one two three" --enableMemH=0
 	echo "Test ARGC_REVMEM: Completed"
-	sst --add-lib-path=../../build/src/ ../rev-basic-config.py -- --program "argc.exe" --args "one two three" --enableMemH=1
+	sst --add-lib-path=../../build/src/ ../rev-model-options-config.py -- --program "argc.exe" --args "one two three" --enableMemH=1
 	echo "Test ARGC_MEMH: Completed"
 else
 	echo "Test ARGC_REVMEM: argc.exe not Found - likely build failed"
