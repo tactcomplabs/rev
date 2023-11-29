1fa27ae1ac216e6a916ee120c80f0986c5e5ede8
Ryan Kabrick <rkabrick@udel.edu>
2023-11-29 15:37:20 -0500

    Making vector.c a cpp file

---
+++ test/syscalls/vector/vector.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+#define assert(x)                                                              \
+  if (!(x)) {                                                                  \
+    asm(".byte 0x00");                                                         \
+    asm(".byte 0x00");                                                         \
+    asm(".byte 0x00");                                                         \
+    asm(".byte 0x00");                                                         \
+  }
+
+int main() {
+  std::vector<int> v;
+  for (int i = 0; i < 100; i++) {
+    v.push_back(i);
+  }
+  for (int i = 0; i < 100; i++) {
+    assert(v[i] == i);
+  }
+  return 0;
+}
