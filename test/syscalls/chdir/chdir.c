597fda5fa8638e3ffa0ec89f198b71ff0df448ff
Ryan Kabrick <rkabrick@udel.edu>
2023-12-05 16:17:40 -0500

    Forgot to change relative path stuff for chdir & clock_gettime

--- test/syscalls/chdir/chdir.c
+++ test/syscalls/chdir/chdir.c
@@ -1,13 +1,13 @@
-#include <unistd.h>
+#include "syscalls.h"
 #include <stdio.h>
-#include "../../../common/syscalls/syscalls.h"
+#include <unistd.h>
 
 int main() {
   /*
    * NOTE: If you pass the directory string directly to `chdir`
    *       chdir("/tmp") you will get garbage data.
    *       This is a known issue with Text data in assembly
-  */
+   */
   const char dir[5] = "/tmp";
   int result = rev_chdir(&dir[0]);
 
