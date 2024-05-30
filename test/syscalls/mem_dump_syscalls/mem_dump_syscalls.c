cd40596116f0f5eabacb84e06e2a3e22e3d84d59
rkabrick <rkabrick@udel.edu>
2024-05-30 13:31:18 -0400

    Adding syscall mem_dump test

--- test/syscalls/mem_dump_syscalls/mem_dump_syscalls.c
+++ test/syscalls/mem_dump_syscalls/mem_dump_syscalls.c
@@ -5,6 +5,6 @@
 int main() {
   dump_mem_range( 0x50000, 0x55000 );
   dump_mem_range_to_file( "full_mem_dump.txt", 0, 1024 * 1024 * 10 - 1 );
-  dump_stack_to_file( "/tmp/stack_dump.txt" );
+  dump_stack_to_file( "stack_dump.txt" );
   dump_stack();
 }
