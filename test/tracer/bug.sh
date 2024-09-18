d6f9e68e56bfb8daf9263498827c11e5466e29e3
Kenneth Griesser <kgriesser@tclcomplabs.com>
2024-09-17 17:24:56 -0700

    bug test update

--- test/tracer/bug.sh
+++ test/tracer/bug.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-/opt/homebrew/bin/riscv64-unknown-elf-g++ -g -O2 -march=rv64g -DRV64G -I../../common/syscalls -I../include -o  bug.mem.rv64g.cpp.exe bug.c
-/opt/homebrew/bin/riscv64-unknown-elf-objdump -D --source bug.mem.rv64g.cpp.exe > bug.mem.rv64g.cpp.dis
-ARCH=rv64g REV_EXE=bug.mem.rv64g.cpp.exe sst --add-lib-path=../../build/src ./rev-test-tracer.py > bug.log
+/opt/homebrew/bin/riscv64-unknown-elf-g++ -g -O2 -march=rv64g -DRV64G -I../../common/syscalls -I../include -o  bug.exe bug.cpp
+/opt/homebrew/bin/riscv64-unknown-elf-objdump -D --source bug.exe > bug.dis
+ARCH=rv64g REV_EXE=bug.exe sst --add-lib-path=../../build/src ./rev-test-tracer.py > bug.log
 
-# echo postprocessing for rev-fast-printf messages
-#/Users/kgriesser/work/rev/scripts/rev-print.py -l log
-
+#echo postprocessing for rev-fast-printf messages
+#/Users/kgriesser/work/rev/scripts/rev-print.py -l bug.log
