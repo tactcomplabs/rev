e3c4a8d4e8e74b8d47b64a569a59bd3ecf1cd0f7
leekillough <15950023+leekillough@users.noreply.github.com>
2024-09-20 15:26:38 -0500

    Clean up shellcheck warnings

--- test/argc_short/run_argc_short.sh
+++ test/argc_short/run_argc_short.sh
@@ -4,7 +4,7 @@
 make clean && make
 
 # Check that the exec was built...
-if [ -x argc.exe ]; then
+if [[ -x argc.exe ]]; then
         set -e
 	sst --add-lib-path=../../build/src/ ../rev-model-options-config.py -- --program "argc.exe" --args "one" --enableMemH=0
 	echo "Test ARGC_SHORT_REVMEM: Completed"
--- test/cache_2/run_cache_2.sh
+++ test/cache_2/run_cache_2.sh
@@ -4,7 +4,7 @@
 make clean && make
 
 # Check that the exec was built...
-if [ -x cache_2.exe ]; then
+if [[ -x cache_2.exe ]]; then
 	sst --add-lib-path=../../build/src/ ./rev-test-cache2.py
 else
 	echo "Test TEST_CACHE2: cache_test2.exe not Found - likely build failed"
