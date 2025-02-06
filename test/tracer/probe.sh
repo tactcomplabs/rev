44d5d5ad8027e5ff83e65e453fbaee328a8df205
Kenneth Griesser <kgriesser@tclcomplabs.com>
2025-02-05 17:31:08 -0700

    demo update

--- test/tracer/probe.sh
+++ test/tracer/probe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 64-bit
-riscv64-unknown-elf-g++ -g -O0 -march=rv64g -DRV64G -I../../common/syscalls -I../include -o probe.exe probe.c || exit 1
+riscv64-unknown-elf-g++ -g -O0 -mabi=ilp32 -march=rv32i -I../../common/syscalls -I../include -o probe.exe probe.c || exit 1
 riscv64-unknown-elf-objdump -D --source probe.exe > probe.dis || exit 2
 
 ARCH=rv64g_zicntr REV_EXE=probe.exe sst --add-lib-path=../../build/src ./rev-probe.py | tee probe.log
