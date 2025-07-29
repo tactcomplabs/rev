7fff1b28cc683f609afed3ecec2350f300673747
Kenneth Griesser <kgriesser@tclcomplabs.com>
2025-07-29 14:43:10 -0700

    enabled probe for demo

--- test/tracer/probe.sh
+++ test/tracer/probe.sh
@@ -4,7 +4,7 @@
 riscv64-unknown-elf-g++ -g -O0 -mabi=ilp32 -march=rv32i -I../../common/syscalls -I../include -o probe.exe probe.c || exit 1
 riscv64-unknown-elf-objdump -D --source probe.exe > probe.dis || exit 2
 
-ARCH=rv64g_zicntr REV_EXE=probe.exe sst --add-lib-path=../../build/src ./rev-probe.py | tee probe.log
+ARCH=rv64g_zicntr REV_EXE=probe.exe sst --add-lib-path=../../build/src ./rev-probe.py -- --probeStartCycle=1 | tee probe.log
 echo
 echo "######### console messages ########"
 ../../scripts/rev-print.py -l probe.log
